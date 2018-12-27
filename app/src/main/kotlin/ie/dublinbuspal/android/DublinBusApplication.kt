244d1e20bea37435090070ce305275368dd44744
Conor O'Brien <conor.w.ob@gmail.com>
2018-12-27 01:24:17 +0000

    Bump versions

--- app/src/main/kotlin/ie/dublinbuspal/android/DublinBusApplication.kt
+++ app/src/main/kotlin/ie/dublinbuspal/android/DublinBusApplication.kt
@@ -46,7 +46,7 @@ class DublinBusApplication : Application() {
     private fun setupDagger() {
         applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(ApplicationModule(applicationContext))
-                .databaseModule(DatabaseModule(resources.getString(R.string.dublin_bus_database_name))) //TODO check database name of existing app
+                .databaseModule(DatabaseModule(resources.getString(R.string.dublin_bus_database_name)))
                 .networkModule(NetworkModule(
                         resources.getString(R.string.dublin_bus_soap_api_endpoint),
                         resources.getString(R.string.dublin_bus_rest_api_endpoint),
