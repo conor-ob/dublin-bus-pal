94114f44a23b25e9e80ede0c317620a12eb1ffa9
Conor O'Brien <conor.w.ob@gmail.com>
2019-05-09 21:40:04 +0100

    Update libs

--- domain/src/main/kotlin/ie/dublinbuspal/usecase/rss/RssNewsUseCase.kt
+++ domain/src/main/kotlin/ie/dublinbuspal/usecase/rss/RssNewsUseCase.kt
@@ -1,10 +1,8 @@
 package ie.dublinbuspal.usecase.rss
 
 import ie.dublinbuspal.model.rss.RssNews
-import ie.dublinbuspal.model.rss.RssNewsAge
 import ie.dublinbuspal.repository.Repository
 import io.reactivex.Observable
-import java.lang.Exception
 import javax.inject.Inject
 
 class RssNewsUseCase @Inject constructor(
@@ -12,15 +10,7 @@ class RssNewsUseCase @Inject constructor(
 ) {
 
     fun getRssNews(): Observable<List<RssNews>> {
-//        return repository.getAll().map { rssNews -> rssNews.sortedBy { it.age.ageInSeconds } }
-        return Observable.just(listOf(
-                RssNews(
-                        title = "",
-                        link = "",
-                        description = "",
-                        age = RssNewsAge(timestamp = "", ageInSeconds = 0L)
-                )
-        ))
+        return repository.getAll().map { rssNews -> rssNews.sortedBy { it.age.ageInSeconds } }
     }
 
 }
